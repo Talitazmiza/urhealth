d60412a4f0060a218be892640cb88e3d55278a42
Diana Fitri <diana.fitri@uisi.ac.id>
2021-07-29 23:09:53 +0700

    define getprofil route

--- server/routes/pasien.js
+++ server/routes/pasien.js
@@ -1,14 +1,21 @@
 import express from "express";
 const router = express.Router();
 
-import { getAllPatient, createPatient, updatePatient, getGraph, deletePatient} from "../controllers/pasien.js";
+import {
+    getAllPatient,
+    createPatient,
+    updatePatient,
+    getGraph,
+    deletePatient,
+    patientProfile
+} from "../controllers/pasien.js";
 
 router.get("/all", getAllPatient);
 router.get("/alldata", getGraph);
 router.post("/create", createPatient);
 router.put("/update/:id", updatePatient);
 router.delete("/delete/:id", deletePatient);
-
+router.get("/profile/:id", patientProfile);
 
 //router = -
 // metod = POST, url=/pasien/all, fungsi=getAllpasien
