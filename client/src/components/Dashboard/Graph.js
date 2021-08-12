884f82d4bec667faa0a8a154e912861f0a0f2cc8
Diana Fitri <diana.fitri@uisi.ac.id>
2021-08-12 14:41:46 +0700

    nyoba data yang ada historynya

--- client/src/components/Dashboard/Graph.js
+++ client/src/components/Dashboard/Graph.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
-import HGraph, { hGraphConvert, calculateHealthScore } from 'hgraph-react';
-import data2017 from '../../data/2017.json';
+import HGraph, { History, hGraphConvert, calculateHealthScore } from 'new-hgraph';
+// import data2017 from '../../data/2017.json';
 import data2018 from '../../data/2018.json';
-
+import datacoba from '../../data/coba_tanggal.json';
+// import HGraph, {
+//     History,
+//     hGraphConvert,
+//     calculateHealthScore
+// } from 'hgraph-react';
 import '../../data.css';
 
 function Graph() {
+
     const convertDataSet = (data) => {
-        // const newData = JSON.parse(data);
         return data.map(d => {
             const converted = hGraphConvert('male', d.metric, d);
             converted.id = d.metric;
@@ -18,13 +23,14 @@ function Graph() {
                     convertedChild.parentKey = c.parentKey;
                     convertedChild.id = c.metric;
                     return convertedChild;
-                });
+                })
             }
             return converted;
         });
-    };
-    const converted2017 = convertDataSet(data2017);
-    const converted2018 = convertDataSet(data2018);
+    }
+    const converted2017 = convertDataSet(datacoba);
+    console.log(converted2017)
+    // const converted2018 = convertDataSet(data2018);
     // const me = 'aku';
     const yearData = [
         {
@@ -32,34 +38,28 @@ function Graph() {
             data: converted2017,
             score: parseInt(calculateHealthScore(converted2017), 10),
         },
-        {
-            label: '2018',
-            data: converted2018,
-            score: parseInt(calculateHealthScore(converted2018), 10),
-        },
+        // {
+        //     label: '2018',
+        //     data: converted2018,
+        //     score: parseInt(calculateHealthScore(converted2018), 10),
+        // },
     ];
-    console.log(yearData[0].data[0].value);
     const [windowWidth, setwindowWidth] = useState(window.innerWidth);
     const [currentYearData] = useState(yearData[0]);
-    console.log(currentYearData);
-    // this.state = {
-    //   windowWidth: window.innerWidth,
-    //   yearData,
-    //   data: yearData[0],
-    //   historyOpen: false,
-    //   historyData: yearData[0].data[0],
-    // }
+    const [card, setCard] = useState(React.createRef());
+    const [historyData, setHistoryData] = useState(yearData[0].data[0]);
+    const [historyOpen, setHistoryOpen] = useState(false);
 
-    // const card = React.createRef();
+
 
     useEffect(() => {
         updateWindowDimensions();
         window.addEventListener('resize', updateWindowDimensions);
-        // document.addEventListener('mousedown', handleClick);
+        document.addEventListener('mousedown', handleClick);
 
         return function cleanup() {
             window.removeEventListener('resize', updateWindowDimensions);
-            // document.removeEventListener('mousedown', handleClick);
+            document.removeEventListener('mousedown', handleClick);
         };
     }, []);
 
@@ -72,28 +72,28 @@ function Graph() {
     // };
 
     const handlePointClick = (data, event) => {
-        // setHistoryOpen(true);
-        // setHistoryData(data);
+        setHistoryOpen(true);
+        setHistoryData(data);
         console.log(data);
         console.log(event);
     };
 
-    // const handleClick = (e) => {
-    //     if (historyOpen && card.current && !card.current.contains(e.target)) {
-    //         setHistoryOpen(false)
-    //     }
-    // }
+    const handleClick = (e) => {
+        if (historyOpen && card.current && !card.current.contains(e.target)) {
+            setHistoryOpen(false)
+        }
+    }
     // const handlePointClick = (data, event) => {
     //     console.log(data);
     //     console.log(event);
     // }
     const sizeBasedOnWindow = ((windowWidth / 4) * 2);
     const size = sizeBasedOnWindow > 1000 ? 1000 : sizeBasedOnWindow;
-    // const historySize = card.current ? card.current.clientWidth - 20 : 0;
+    const historySize = card.current ? card.current.clientWidth - 20 : 0;
 
     return (
         <>
-            <div className="vis-container">
+            <div className="vis-container" style={{ height: historyOpen ? '50vh' : '100vh' }}>
                 <HGraph
                   data={currentYearData.data}
                   score={currentYearData.score}
@@ -103,7 +103,19 @@ function Graph() {
                   pointRadius={size < 300 ? 5 : 10}
                   scoreFontSize={size < 300 ? 50 : 120}
                   onPointClick={handlePointClick}
+                  zoomOnPointClick={false}
                 />
+            </div>
+            <div className="card" style={{ top: historyOpen ? '50vh' : '100vh' }} ref={card}>
+                <div>
+                    <p>{ historyData.label }</p>
+                    <p>{ historyData.value } { historyData.unitLabel }</p>
+                    <History
+                        width={ historySize }
+                        height={ historySize > 500 ? historySize / 4 : historySize / 2 }
+                        data={historyData}
+                    />
+                </div>
             </div>
         </>
     );
