b4f9799a5de03164484f269f5950cd6054e9da01
Diana Fitri <diana.fitri@uisi.ac.id>
2021-08-12 22:51:04 +0700

    update model jadi data yg ada tangggalnya

--- server/models/data.js
+++ server/models/data.js
@@ -3,10 +3,15 @@
 // export default mongoose.model("graphdata", new mongoose.Schema({}), "graphdata");
 
 import mongoose from "mongoose";
-var data_object_grafik = mongoose.Schema( {
-    metric: {type: String, required: true},
+var object_grafik_inner = mongoose.Schema( {
+    date: { type: Date, default: Date.now, required: true },
     value: {type: Number, required: true},
 }, { _id : false });
+var data_object_grafik = mongoose.Schema( {
+    metric: {type: String, required: true},
+    values: [object_grafik_inner],
+}, { _id : false });
+
 const grafikSchema = mongoose.Schema({
         id: {type: String},
         data_grafik: [data_object_grafik],
